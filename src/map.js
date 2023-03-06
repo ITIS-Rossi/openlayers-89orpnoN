e9648380aedd05c1dc11c2ba93708c8ff1bab8ff
89orpnoN <9698022@itisrossi.vi.it>
2023-03-06 09:34:00 +0100

    Messe istruzoni per fare il marker

--- src/map.js
+++ src/map.js
@@ -2,9 +2,11 @@
 
 function init() {
   ol.proj.useGeographic()
+  markerLayer = new ol.layer.VectorLayer()
   let map = new ol.Map({
     layers: [
       new ol.layer.Tile({source: new ol.source.OSM()}), 
+      markerLayer
     ],
     view: new ol.View({
       center: [11.555649865625005,45.55247714363049],
@@ -22,10 +24,18 @@ let MarkerLayer = new ol.layer.Vector({
     })
   })
 });
-
+let markerstyle = ol.style.Style({image:ol.style.icon({src:"Media\Img\marker.png"}))
+let marker = new ol.Feature({geometry: new ol.geom.point([2.2931, 48.8584]), name:"rossimarker", description:"boh rossimarker"})
+marker.setStyle(markerstyle)
+markerSource = markerLayer.getSource()
 console.log(MarkerLayer)
 map.addLayer(MarkerLayer)
-var marker = new ol.Feature(new ol.geom.Point(ol.proj.fromLonLat([2.2931, 48.8584])));
 MarkerLayer.getSource().addFeature(marker)
 }
 init()
+
+//crea una nuova feature: new ol.feature() devi specificare geometry con funzione new ol.geom.point(posizione), nome e decrizione.
+//crea un nuova style con new ol.style.Style() e crea una nuova icona con ol.style.icon()
+//aggiungi lo style alla feature 
+//aggiungi la feature a una vectorsource
+//vectorsource si crea con new ol.vector.source
