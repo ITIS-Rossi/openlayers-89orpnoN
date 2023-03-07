8df45416fa712a968f84f772e99402bf64b96f9b
89orpnoN <115448007+89orpnoN@users.noreply.github.com>
2023-03-07 13:32:36 +0100

    non ho tempo di scrivere

--- src/map.js
+++ src/map.js
@@ -2,7 +2,26 @@
 
 function init() {
   ol.proj.useGeographic()
-  markerLayer = new ol.layer.VectorLayer()
+
+  markerStyle = new ol.style.Style({
+    image: new ol.style.Icon({
+      src: "Media\\Img\\marker.png",
+      anchor: [0.5,1],
+      scale: 0.5
+      })
+    })
+
+  markerLayer = new ol.layer.Vector({
+    source: new ol.source.Vector({
+    features:[
+      new ol.Feature({
+        geometry: new ol.geom.Point([11.555649865625005,45.55247714363049]),
+	style: markerStyle 
+        })
+      ]
+    })
+  })
+
   let map = new ol.Map({
     layers: [
       new ol.layer.Tile({source: new ol.source.OSM()}), 
@@ -10,27 +29,15 @@ function init() {
     ],
     view: new ol.View({
       center: [11.555649865625005,45.55247714363049],
-      zoom: 19,
+      zoom: 19
     }),
     target: 'map',
   });
 
-let MarkerLayer = new ol.layer.Vector({
-  source: new ol.source.Vector(),
-  style: new ol.style.Style({
-    image: new ol.style.Icon({
-      anchor: [0.5, 1],
-      src: 'marker.png'
-    })
-  })
-});
-let markerstyle = ol.style.Style({image:ol.style.icon({src:"Media\Img\marker.png"}))
-let marker = new ol.Feature({geometry: new ol.geom.point([2.2931, 48.8584]), name:"rossimarker", description:"boh rossimarker"})
-marker.setStyle(markerstyle)
-markerSource = markerLayer.getSource()
-console.log(MarkerLayer)
-map.addLayer(MarkerLayer)
-MarkerLayer.getSource().addFeature(marker)
+
+
+
+
 }
 init()
 
